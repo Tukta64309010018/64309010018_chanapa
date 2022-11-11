9fa82e56de5f060ff2e9bf517efdcfa3be38ed15
Chanapa Siriphan <baitoey2002gt@gmail.com>
2022-11-11 13:31:39 +0700

    Create Exam1_64309010018.js

---
+++ Week4/TestJS.js
@@ -0,0 +1,13 @@
+var myName = 'Chanapa';
+console.log(myName);
+let lastName = 'Siriphan';
+
+const age = 19;
+//age = 20;
+var year = 2022;
+let grade =  3.41;
+
+grad = Math.floor(grade);
+console.log(grade);
+
+console.log(`My name is ${myName}`);
